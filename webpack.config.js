2275eebc57060d21a01c88677b0ad5774c8cbe43
F Adonis <fadonis@takima.fr>
2018-07-13 15:32:03 +0200

    fix: removed some comments

--- webpack.config.js
+++ webpack.config.js
@@ -35,26 +35,7 @@ module.exports = {
 
           {
             loader: require.resolve('sass-loader'),
-          },
-
-          // {
-          //   loader: require.resolve('postcss-loader'),
-          //   options: {
-          //     ident: 'postcss',
-          //     plugins: () => [
-          //       require('postcss-flexbugs-fixes'),
-          //       autoprefixer({
-          //         browsers: [
-          //           '>1%',
-          //           'last 4 versions',
-          //           'Firefox ESR',
-          //           'not ie < 9',  
-          //         ],
-          //         flexbox: 'no-2009',
-          //       }),
-          //     ],
-          //   },
-          // }
+          }
         ]
       }
     ]
